d6919cbdff12f2ad96bd42c81c2b9fc46ecfdccf
Niklas Hedlund <nojan1989@gmail.com>
2016-03-07 21:58:15 +0100

    Read .projectinfo.json

--- server/app/project.py
+++ server/app/project.py
@@ -1,5 +1,5 @@
 from git import Repo
-import config, os
+import config, os, json
 
 class Project(object):
     def __init__(self, projectId, projectName = ""):
@@ -9,9 +9,20 @@ class Project(object):
             self._repo = Repo(repoPath)
         else:
             self._repo = Repo.init(repoPath)
-            self.initializeRepo(projectName)
+            self.initializeRepo(repoPath, projectName)
         
         self.projectId = projectId
+        
+        infoFilePath = os.path.join(repoPath, ".projectinfo.json")
+        if not os.path.exists(infoFilePath):
+            raise Exception("Repo does not contain .projectinfo.json")
+
+        with open(infoFilePath, "r") as infile:
+            obj = json.load(infile)
+            print(obj)
+            self.projectName = obj["projectName"]
+            
+        self.lastAccess = self._repo.head.commit.committed_date
 
     def getFiles():
         pass
@@ -19,8 +30,15 @@ class Project(object):
     def getNotes():
         pass
         
-    def initializeRepo(self, projectName):
-        pass
+    def initializeRepo(self, repoPath, projectName):
+        infoFilePath = os.path.join(repoPath, ".projectinfo.json")
+        with open(infoFilePath, "w") as outfile:
+            json.dump({"projectName": projectName}, outfile, indent = 4)
+            
+        self._repo.index.add(".projectinfo.json")
+        self._repo.index.commit("initial commit")
+            
+        
 
         
         
