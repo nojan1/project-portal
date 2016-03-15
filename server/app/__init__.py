750ded7864172bff5f919110e0983ddaeec65efc
Niklas Hedlund <nojan1989@gmail.com>
2016-03-15 13:25:10 +0100

    Added dependency injection server side

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,20 +1,40 @@
 from flask import Flask
 from flask.ext.restful import Api
+from flask_injector import FlaskInjector
+from injector import inject, Module, provides, singleton
 
 import config
 from projectApi import *
+from projectRepository import ProjectRepository
 
-app = Flask(__name__, static_url_path="")
-api = Api(app)
+def configure_ext(binder):
+    binder.bind(ProjectRepository, to=ProjectRepository(), scope=singleton)
 
-@app.after_request
-def after_request(response):
-  response.headers.add('Access-Control-Allow-Origin', '*')
-  response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With')
-  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-  return response
+@inject(app=Flask)
+def configure_api(binder, app):
+        api = Api(app)
+        api.add_resource(ProjectListAPI, config.API_PATH_PREFIX + "/projects")
+        
+        binder.bind(Api, to=api, scope=singleton)
 
-api.add_resource(ProjectListAPI, config.API_PATH_PREFIX + "/projects")
+def main(debug):
+    app = Flask(__name__)
+    # app.config.update(
+    #     debug=debug,
+    # )
 
+    # attach your views etc. here
+
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With')
+        response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+        return response
+
+    FlaskInjector(app=app, modules=[configure_api, configure_ext])
+    
+    app.run(debug=debug)
+    
 if __name__ == "__main__":
-    app.run(debug=True)
+    main(True)
--- server/app/projectApi.py
+++ server/app/projectApi.py
@@ -1,6 +1,9 @@
 from flask.ext.restful import Resource, fields, marshal, reqparse
 from projectRepository import *
 
+from flask_injector import FlaskInjector
+from injector import inject
+
 project_fields = {
     'projectId': fields.String,
     'projectName': fields.String,
@@ -8,18 +11,20 @@ project_fields = {
 }
 
 class ProjectListAPI(Resource):
-     def __init__(self):
+     @inject(projectRepository=ProjectRepository)
+     def __init__(self, projectRepository):
+        self.projectRepository = projectRepository
         super(ProjectListAPI, self).__init__()
         
      def get(self):
-        return [marshal(project, project_fields) for project in ProjectRepository().getProjects()]
+        return [marshal(project, project_fields) for project in self.projectRepository.getProjects()]
         
      def post(self):
         self.reqparse = reqparse.RequestParser()
         self.reqparse.add_argument('projectName', type = str, required = True, location = 'json')
         
         args = self.reqparse.parse_args()
-        print(args)
-        project = ProjectRepository().createProject(args["projectName"])
+        
+        project = self.projectRepository.createProject(args["projectName"])
         
         return marshal(project, project_fields)
