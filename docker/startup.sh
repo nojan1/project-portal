443bdc10b1ff77bee0c6481d74c9df1d0f7f511e
Niklas Hedlund <nojan1989@gmail.com>
2016-03-15 15:55:34 +0100

    Do not use default http port

--- docker/startup.sh
+++ docker/startup.sh
@@ -2,10 +2,10 @@
 
 APP_ROOT="/srv"
 
-exec uwsgi --socket 0.0.0.0:80 \
+exec uwsgi --socket 0.0.0.0:3031 \
 		   --chdir $APP_ROOT/server \
 		   --wsgi-file server/WSGI.py  \
 		   --master --processes 4 --threads 2 \
-		   --stats 0.0.0.0:8080 \
+		   --stats 0.0.0.0:3032 \
 		   --check-static $APP_ROOT/client/ \
            --static-index index.html
